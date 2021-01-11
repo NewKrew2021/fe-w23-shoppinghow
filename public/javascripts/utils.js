42011a869d6058e8ba93850e58eb24175b9b61ef
puba.yu <puba.yu@kakaocorp.com>
2021-01-11 22:49:10 +0900

    feat : custom querySelector 함수 생성
    - 커스텀한 querySelector 함수 생성
    - 재귀적으로 탐색, DFS 알고리즘을 활용
    - 조건(id, class, tagName)을 나누어 실행

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,3 +1,25 @@
+const customQuerySelector = (currentElement = document.body, target) => {
+  switch (target[0]) {
+    case '.':
+      console.log(currentElement.className);
+      if (currentElement.className === target.substr(1)) return currentElement;
+      break;
+    case '#':
+      console.log(currentElement.id);
+      if (currentElement.id === target.substr(1)) return currentElement;
+      break;
+    default:
+      console.log(currentElement.tagName);
+      if (currentElement.tagName.toLowerCase() === target)
+        return currentElement;
+  }
+  for (let childElement of currentElement.children) {
+    const searchResult = customQuerySelector(childElement, target);
+    if (searchResult !== null) return searchResult;
+  }
+  return null;
+};
+
 const $ = (target, parent = document) => {
   return parent.querySelector(target);
 };
