b112ad9ceb588d61c50c85756c369c71480737e1
puba.yu <puba.yu@kakaocorp.com>
2021-01-11 11:32:40 +0900

    feat : index 페이지에 대한 라우팅 추가
    - koa-router을 이용하여 라우팅
    - index page에 대한 라우팅 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const Koa = require('koa');
-const app = new Koa();
-const PORT = 4000;
+const Router = require('koa-router');
 
-app.use(ctx => {
-  ctx.body = 'Hello World';
+const PORT = 8000;
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'index page';
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(PORT, () => {
   console.log(`Server is listening to port http://localhost:${PORT}`);
