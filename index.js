4b4adbf2c09e9eeb3dddfdcf49130565cc918e1e
Geonho Kim <gunhoflash@gmail.com>
2021-01-11 18:13:24 +0900

    Add router

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // refer: https://expressjs.com/ko/starter/hello-world.html
 const express = require('express')
 const app = express()
+const routes = require('./lib/routes')
 const port = 3000
 
 // set root directories to serve static files
 app.use(express.static('public'))
 app.use(express.static('views'))
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+// use router
+app.use('/', routes)
 
 // start the server
 app.listen(port, () => {
