f6568c4ad67fa6739b24e974e79f73ff8a283cac
tjrdud123 <youmr100@naver.com>
2021-01-11 10:53:21 +0900

    init: 환경 구축 - 폴더구조, 빈파일 생성 - express, ejs 모듈 설치

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+const server = app.listen(3000, function() {
+    console.log("Express server has started on port 3000");
+});
+
+app.use(express.static('public'));
+app.use(express.json());
+
+const router = require("./router/router.js")(app, fs);
