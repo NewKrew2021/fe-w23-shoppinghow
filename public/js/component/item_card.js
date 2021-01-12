a5f429bea7a5fb61098103f6aa26628169799fdd
Geonho Kim <gunhoflash@gmail.com>
2021-01-12 17:49:06 +0900

    Add a component generator for item card

---
+++ public/js/component/item_card.js
@@ -0,0 +1,19 @@
+// append a simple image-carousel to the wrapper
+function appendItemCard(wrapper, items) {
+  const ItemCardHtmlString = items.reduce(
+    (str, { href, src, title, subtitle, badge }) => str + `
+    <div class="box w-20 p-3">
+      <a href="${href}">
+        <img src="${src}">
+        <small class="d-block mt-3">
+          <h3 class="text-ellipsis mb-1">${title}</h3>
+          <p class="text-ellipsis my-1 gray">${subtitle}</p>
+          <span class="mt-2 badge">${badge}</span>
+        </small>
+      </a>
+    </div>
+    `, '')
+
+  // append to wrapper
+  wrapper.innerHTML += ItemCardHtmlString
+}
