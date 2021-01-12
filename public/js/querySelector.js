f4da064d2e13ccee86f1003b12c4999b62a21009
justin.bak <justin.bak@kakaocorp.com>
2021-01-12 19:36:10 +0900

    Add querySelector and querySelectorAll
    querySelector 와 querySelectorAll 을class 형태로 구현하였습니다.

---
+++ public/js/querySelector.js
@@ -0,0 +1,48 @@
+class querySelector{
+
+    #dfs(base,selector){
+        const childNodes=base.childNodes;
+        for( let child of childNodes){
+            if(typeof child.tagName === 'undefined') continue;
+            if(child.matches(selector)) {
+                return child;
+            }else{
+                let result = this.#dfs(child,selector);
+                if(typeof result !== 'undefined') return result;
+            }        
+        }
+    }
+    
+    query(base, selector){
+        return this.#dfs(base,selector);
+    };
+
+}
+
+class querySelectorAll{
+    #result;
+    #dfsAll(base,selector,type){
+        const childNodes=base.childNodes;
+        for( let child of childNodes){
+            if(typeof child.tagName === 'undefined') continue;
+            if(child.matches(selector)) this.#result.push(child);
+            this.#dfsAll(child,selector);
+        }
+    }
+
+    query(base,selector){
+        this.#result=[];
+        this.#dfsAll(base,selector);
+        return this.#result;
+    }
+}
+/** [usage] :
+const qs = new querySelector();
+console.log(qs.query(document,"img"));
+console.log(qs.query(document,"#search-bar"));
+console.log(qs.query(document,".btn"));
+const qsa = new querySelectorAll();
+console.log(qsa.query(document,"img"));
+console.log(qsa.query(document,"#search-bar"));
+console.log(qsa.query(document,".btn"));
+*/
