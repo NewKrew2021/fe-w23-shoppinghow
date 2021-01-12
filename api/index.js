3bae07113bc47b5436061958918bfdab131198a6
puba.yu <puba.yu@kakaocorp.com>
2021-01-12 14:26:30 +0900

    feat : JSON data를 가져오도록 경로 설정
    - 임의의 JSON 파일을 만든 후 경로 설정

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const Router = require('koa-router');
+const carouselData = require('../data/carousel.json');
 
 const api = new Router();
 
 api.get('/carousel', (ctx, next) => {
+  console.log(carouselData);
   ctx.body = 'GET ' + ctx.request.path;
 });
 
