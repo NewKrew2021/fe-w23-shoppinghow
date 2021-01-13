63b2a964fc8029e2a94e2d386a907022a13dba9e
puba.yu <puba.yu@kakaocorp.com>
2021-01-13 18:49:42 +0900

    feat : product API 추가

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 const Router = require('koa-router');
 const carouselData = require('../data/carousel.json');
+const productItemListData = require('../data/product-item-list.json');
 
 const api = new Router();
 
 api.get('/carousel', (ctx, next) => {
   ctx.body = carouselData;
+});
+
+api.get('/product', (ctx, next) => {
+  ctx.body = productItemListData;
 });
 
 module.exports = api;
