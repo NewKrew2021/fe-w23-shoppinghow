60139cb077cc2e628bf2749956149072cf4dba54
석찬희 <roddy@roddy-pc.local>
2021-01-14 20:56:21 +0900

    Add MyPromise class Promise 객체를 직접 구현한 MyPromise class를 생성하였습니다. 아직은 일부 기능만 동작하고, 좀 더 공부한 뒤 상세 구현을 할 예정입니다.

--- public/js/hotSlide.js
+++ public/js/hotSlide.js
@@ -2,7 +2,6 @@
     hotSlide.js
     지금 뜨는 테마 카테고리 슬라이드 클래스
 */
-
 class HotSlide{
     constructor() {
         /* 많은 DOM 요소들 */
@@ -41,33 +40,14 @@ class HotSlide{
             e.target.src='/images/next2_press_btn.png';
             this.mousedownTime = new Date().getTime();
         });
-        //this.slidePrevBtn.addEventListener('click', ()=> this.prevBtnClickHandler());
-        //this.slideNextBtn.addEventListener('click', ()=> this.nextBtnClickHandler());
     }
 
     /* 이전, 다음 버튼 클릭 이벤트 */
-    prevBtnClickHandler() { 
-        this.slideList.style.transition = SLIDE_SPEED + "ms";
-        this.slideList.style.transform =
-            "translate3d(-" + (HOT_SLIDE_WIDTH * (h_curSlideIndex + 4)) + "px, 0px, 0px)";
-        
-        // 첫 번째 슬라이드에 도달했을 경우, 맨 끝으로 돌아가게 한다.
-        if (h_curSlideIndex === -4) {
-            setTimeout(() => {
-                this.slideList.style.transition = "0ms";
-                this.slideList.style.transform =
-                    "translate3d(-" + (HOT_SLIDE_WIDTH * this.slideLength) + "px, 0px, 0px)";
-            }, AUTO_SLIDE_SPEED);
-            h_curSlideIndex = 6;
-        }
-        this.curSlide = this.slideContent[--h_curSlideIndex];
-    }
-
+    /* 2초(임시 : 1.5초)보다 짧게 눌렀으면 한 칸만, 아니면 두 칸을 이동하게 함 */
     PrevBtnPressHandler(e){
         let mouseupTime = new Date().getTime(),
               timeDifference = mouseupTime - this.mousedownTime;
         console.log(timeDifference);
-        /* 2초(임시 : 1.5초)보다 짧게 눌렀으면 한 칸만, 아니면 두 칸을 이동하게 함 */
 
         /* 버튼 이미지 변경 */
         e.target.src='/images/prev2_btn.png';
@@ -107,32 +87,12 @@ class HotSlide{
         }
     }
 
-    nextBtnClickHandler() {
-        if (h_curSlideIndex <= this.slideLength - 1) {
-            this.slideList.style.transition = SLIDE_SPEED + "ms";
-            this.slideList.style.transform =
-                "translate3d(-" + (HOT_SLIDE_WIDTH * (h_curSlideIndex + 6)) + "px, 0px, 0px)";
-        }
-        // 마지막 슬라이드에 도달했을 경우, 맨 처음으로 돌아가게 한다.
-        if (h_curSlideIndex === this.slideLength - 1) {
-            setTimeout(() =>{
-                this.slideList.style.transition = "0ms";
-                this.slideList.style.transform = "translate3d(-" + (HOT_SLIDE_WIDTH * (h_curSlideIndex + 5)) + "px, 0px, 0px)";
-            }, AUTO_SLIDE_SPEED);
-            h_curSlideIndex = -1;
-        }
-        this.curSlide = this.slideContent[++h_curSlideIndex];
-    }
-
     /* 다음 버튼을 2초 누를 시 발생하는 이벤트 핸들러 */
     nextBtnPressHandler(e){
         let mouseupTime = new Date().getTime(),
               timeDifference = mouseupTime - this.mousedownTime;
-        console.log(timeDifference);
-
         e.target.src='/images/next2_btn.png';
 
-        /* 2초(임시 : 1.5초)보다 짧게 눌렀으면 한 칸만, 아니면 두 칸을 이동하게 함 */
         if (timeDifference < 1500){
             if (h_curSlideIndex <= this.slideLength - 1) {
                 this.slideList.style.transition = SLIDE_SPEED + "ms";
@@ -172,9 +132,5 @@ class HotSlide{
         /* 실행 메서드 */
         this.onReady();
         this.onEvents();
-    }
-
-    initTest(){
-        this.onReady();
     }
 }
