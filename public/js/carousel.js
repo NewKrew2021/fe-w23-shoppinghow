f631fee5e0aeec866243201bbf37b77b4491bab1
puba.yu <puba.yu@kakaocorp.com>
2021-01-14 12:52:19 +0900

    feat : Carousel 함수 틀 생성
    - 캐로셀 함수 틀 생성

---
+++ public/js/carousel.js
@@ -0,0 +1,13 @@
+class Carousel {
+  constructor() {}
+  // 오른쪽으로 이동
+  moveToRight() {}
+  // 왼쪽으로 이동
+  moveToLeft() {}
+  // 버튼 이벤트 등록
+  addButtonEvent() {}
+  // 생성
+  init() {}
+}
+
+export { Carousel };
