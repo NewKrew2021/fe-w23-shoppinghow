cca9a10ecba945ecfeaa524d44919629f84a4a28
puba.yu <puba.yu@kakaocorp.com>
2021-01-14 15:17:40 +0900

    feat : Carousel을 만드는 클래스 생성
    - Carousel의 ProductList를 만드는 함수 생성

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,5 +1,42 @@
 class Carousel {
   constructor() {}
+
+  createProductListTemplate(productItemList) {
+    const productListContent = productItemList.reduce(
+      (acc, { image, title, description, tagList }) =>
+        acc +
+        `<li class="product-item">
+              <img
+                class="product-item__image"
+                src="${image}"
+              />
+              <div class="product-item__title">${title}</div>
+              <div class="product-item__description">
+                ${description}
+              </div>
+              <div class="product-tag-list">
+                <div class="product-tag">
+                ${this.createTagListTemplate(tagList)}
+                </div>
+              </div>
+            </li>`,
+      ''
+    );
+
+    return `<ul class="product-list__row">${productListContent}</ul>`;
+  }
+
+  createCarouselProductList = () => {
+    this;
+    return `
+        <div class="carousel-product-list">
+            <div class="carousel-product-list__before">${this.createProductListTemplate()}</div>
+            <div class="carousel-product-list__current">${this.createProductListTemplate()}</div>
+            <div class="carousel-product-list__after">${this.createProductListTemplate()}</div>
+        </div>;
+        `;
+  };
+
   // 오른쪽으로 이동
   moveToRight() {}
   // 왼쪽으로 이동
