34072c00204daede34a6c59ee371cb97114523c7
석찬희 <roddy@roddy-pc.local>
2021-01-15 18:27:16 +0900

    etc : fix slider class slider class의 일부 오류를 수정하였습니다.

--- public/js/slider.js
+++ public/js/slider.js
@@ -35,7 +35,7 @@ class Slider {
         this.slideList.insertBefore(this.copyLastSlide, this.slideList.firstElementChild);
         this.slideList.style.width = (this.SLIDE_WIDTH) * (this.slideLength * 2) + "px";
         this.slideList.style.transform =
-            "translate3d(-" + ((this.SLIDE_WIDTH) * (curSlideIndex + this.SHOW_LENGTH)) + "px, 0px, 0px)";
+            "translate3d(-" + ((this.SLIDE_WIDTH) * (this.curSlideIndex + this.SHOW_LENGTH)) + "px, 0px, 0px)";
     }
 
     onEvents() {
@@ -51,57 +51,57 @@ class Slider {
 
     /* 페이지 내비게이터가 있을 경우에만 동작 */
     prevPageNavigator() {
-        this.pagination[(curSlideIndex === this.slideLength) ? 0 :
-            curSlideIndex].classList.remove('active');
-        this.curSlide = this.slideContent[--curSlideIndex];
-        this.pagination[curSlideIndex].classList.add('active');
+        this.pagination[(this.curSlideIndex === this.slideLength) ? 0 :
+            this.curSlideIndex].classList.remove('active');
+        this.curSlide = this.slideContent[--this.curSlideIndex];
+        this.pagination[this.curSlideIndex].classList.add('active');
     }
 
     nextPageNavigator() {
-        this.pagination[(curSlideIndex === -1) ? this.slideLength - 1 :
-            curSlideIndex].classList.remove('active');
-        this.curSlide = this.slideContent[++curSlideIndex];
-        this.pagination[curSlideIndex].classList.add('active');
+        this.pagination[(this.curSlideIndex === -1) ? this.slideLength - 1 :
+            this.curSlideIndex].classList.remove('active');
+        this.curSlide = this.slideContent[++this.curSlideIndex];
+        this.pagination[this.curSlideIndex].classList.add('active');
     }
 
     /* 이전, 다음 버튼 클릭 이벤트 */
     prevBtnClickHandler() {
-        if (curSlideIndex >= 0) {
+        if (this.curSlideIndex >= 0) {
             this.slideList.style.transition = this.SLIDE_SPEED + "ms";
             this.slideList.style.transform =
-                "translate3d(-" + (this.SLIDE_WIDTH * (curSlideIndex)) + "px, 0px, 0px)";
+                "translate3d(-" + (this.SLIDE_WIDTH * (this.curSlideIndex)) + "px, 0px, 0px)";
         }
         // 첫 번째 슬라이드에 도달했을 경우, 맨 끝으로 돌아가게 한다.
-        if (curSlideIndex === 0) {
+        if (this.curSlideIndex === 0) {
             setTimeout(() => {
                 this.slideList.style.transition = "0ms";
                 this.slideList.style.transform =
                     "translate3d(-" + (this.SLIDE_WIDTH * this.slideLength) + "px, 0px, 0px)";
             }, this.AUTO_SLIDE_SPEED);
-            curSlideIndex = this.slideLength;
+            this.curSlideIndex = this.slideLength;
         }
 
         if(this.pagination !== undefined)
-            prevPageNavigator();
+            this.prevPageNavigator();
     }
 
     nextBtnClickHandler() {
-        if (curSlideIndex <= this.slideLength - 1) {
+        if (this.curSlideIndex <= this.slideLength - 1) {
             this.slideList.style.transition = this.SLIDE_SPEED + "ms";
             this.slideList.style.transform =
-                "translate3d(-" + (this.SLIDE_WIDTH * (curSlideIndex + (this.SHOW_LENGTH + 1))) + "px, 0px, 0px)";
+                "translate3d(-" + (this.SLIDE_WIDTH * (this.curSlideIndex + (this.SHOW_LENGTH + 1))) + "px, 0px, 0px)";
         }
         // 마지막 슬라이드에 도달했을 경우, 맨 처음으로 돌아가게 한다.
-        if (curSlideIndex === this.slideLength - 1) {
+        if (this.curSlideIndex === this.slideLength - 1) {
             setTimeout(() => {
                 this.slideList.style.transition = "0ms";
                 this.slideList.style.transform = "translate3d(-" + (this.SLIDE_WIDTH) + "px, 0px, 0px)";
             }, this.AUTO_SLIDE_SPEED);
-            curSlideIndex = -1;
+            this.curSlideIndex = -1;
         }
 
         if(this.pagination !== undefined)
-            nextPageNavigator();
+            this.nextPageNavigator();
     }
 
     /* (필요할 경우에만) 내비게이터 마우스 오버 이벤트 */
@@ -115,7 +115,7 @@ class Slider {
         this.slideList.style.transition = this.SLIDE_SPEED + "ms";
         this.slideList.style.transform =
             "translate3d(-" + (this.SLIDE_WIDTH) * (index + 1) + "px, 0px, 0px)";
-        curSlideIndex = index;
+        this.curSlideIndex = index;
     }
 
     init() {
