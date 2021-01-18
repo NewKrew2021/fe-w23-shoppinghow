8c3e611ffcd7dea994b12340573c75b27cdce8b1
석찬희 <roddy@roddy-pc.local>
2021-01-18 12:07:10 +0900

    Add category.js for menu layer 카테고리 탭의 메뉴 표시를 담당하는 파일을 생성하였습니다.

--- public/js/keyword.js
+++ public/js/keyword.js
@@ -3,7 +3,7 @@
     쇼핑하우 검색창 클릭 시 나타나는 추가 영역 관련 함수
 */
 
-/* 검색창 포커스 시 keyword 영역 표시 */
+/* 검색창 포커스 시 keyword 영역 표시, 포커스 해제 시 none */
 function showKeywordInner(){
     const search_blank = dom('.search-input').querySelector();
     const keyword = dom('.keyword').querySelector();
