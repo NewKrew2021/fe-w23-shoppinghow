8713ab5097ff0154b7b8f45de9741db7041bdba2
석찬희 <roddy@roddy-pc.local>
2021-01-18 14:58:59 +0900

    Change filename : keyword.js to searchbox.js 추후 모듈화를 위해 검색창 관련 파일명을 변경하였고 클래스 생성 준비를 하였습니다.

--- public/js/searchbox.js
+++ public/js/searchbox.js
@@ -1,7 +1,12 @@
 /*
-    keyword.js
-    쇼핑하우 검색창 클릭 시 나타나는 추가 영역 관련 함수
+    searchbox.js
+    쇼핑하우 검색창 관련 클래스
 */
+
+// 클래스화 - 모듈
+class SearchBox{
+
+}
 
 /* 검색창 포커스 시 keyword 영역 표시, 포커스 해제 시 none */
 function showKeywordInner(){
@@ -12,9 +17,13 @@ function showKeywordInner(){
     const search_input = dom('.search-input').querySelector();
     search_input.addEventListener('input',(e)=>{
         const value = e.target.value;
-        if(value === ''){ // 빈 칸일 경우 다시 인기 키워드를 표시
+        if(value === ''){
             auto_inner.style.display = 'none';
             keyword_inner.style.display = 'block';
+        }
+        else{
+            auto_inner.style.display = 'block';
+            keyword_inner.style.display = 'none';
         }
     });
     search_blank.addEventListener('focus', ()=>{
@@ -22,20 +31,6 @@ function showKeywordInner(){
     })
     search_blank.addEventListener('blur',()=>{
         keyword.style.display = 'none';
-        keyword_inner.style.display = 'block';
-        auto_inner.style.display = 'none';
     })
 }
 showKeywordInner();
-
-/* 단어 입력 시 자동완성 영역 표시 */
-function showAutocomplete(){
-    const search_blank = dom('.search-input').querySelector();
-    const keyword_inner = dom('.keyword-inner').querySelector();
-    const auto_inner = dom('.auto-complete').querySelector();
-    search_blank.addEventListener('keydown',()=>{
-        keyword_inner.style.display = 'none';
-        auto_inner.style.display = 'block';
-    })
-}
-showAutocomplete();
