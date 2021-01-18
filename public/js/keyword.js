b37016cb53fc624ae4ee23b2f0a98326b3f923dc
석찬희 <roddy@roddy-pc.local>
2021-01-18 12:04:23 +0900

    feature : Add keyword layout 검색창에 포커스를 두면 하단의 키워드 영역이 나오는 이벤트를 추가하였습니다.

---
+++ public/js/keyword.js
@@ -0,0 +1,17 @@
+/*
+    keyword.js
+    쇼핑하우 검색창 클릭 시 나타나는 추가 영역 관련 함수
+*/
+
+/* 검색창 포커스 시 keyword 영역 표시 */
+function showKeywordInner(){
+    const search_blank = dom('.search-input').querySelector();
+    const keyword = dom('.keyword').querySelector();
+    search_blank.addEventListener('focus', ()=>{
+        keyword.style.display = 'block';
+    })
+    search_blank.addEventListener('blur',()=>{
+        keyword.style.display = 'none';
+    })
+}
+showKeywordInner();
