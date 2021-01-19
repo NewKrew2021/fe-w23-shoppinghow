f594dc706f7fffde367ba4ca13c5a5dadf31a0c7
puba.yu <puba.yu@kakaocorp.com>
2021-01-19 16:08:53 +0900

    feat : 중분류, 소분류를 만드는 매소드 생성 및 init 매소드 생성 및 실행
    - 중분류, 소분류를 만드는 매소드 생성
    - init 매소드 생성
    - init 매소드를 main에 가져와 실행

--- public/js/components/menu.js
+++ public/js/components/menu.js
@@ -1,11 +1,38 @@
+import { $ } from '../utils.js';
+
 const LARGE_CATEGORY = ['패션 / 뷰티', '가전 / 컴퓨터', '가구 / 생활 / 건강'];
+const MEDIUM_CATEGORY = ['패션 / 뷰티', '가전 / 컴퓨터', '가구 / 생활 / 건강'];
+const SMALL_CATEGORY = ['패션 / 뷰티', '가전 / 컴퓨터', '가구 / 생활 / 건강'];
 
 class Menu {
-  constructor() {}
+  constructor() {
+    this.largeCategoryElement = $('.large-category');
+    this.mediumCategoryElement = $('.medium-category');
+    this.smallCategoryElement = $('.small-category');
+  }
+
   createLargeCategoryElement() {
     return LARGE_CATEGORY.reduce((acc, largeCategory) => {
       return acc + `<li class="large-category__tab">${largeCategory}</li>`;
     }, '');
+  }
+
+  createMediumCategoryElement() {
+    return MEDIUM_CATEGORY.reduce((acc, largeCategory) => {
+      return acc + `<li class="medium-category__tab">${largeCategory}</li>`;
+    }, '');
+  }
+
+  createSmallCategoryElement() {
+    return SMALL_CATEGORY.reduce((acc, largeCategory) => {
+      return acc + `<li class="small-category__tab">${largeCategory}</li>`;
+    }, '');
+  }
+
+  init() {
+    this.largeCategoryElement.innerHTML = this.createLargeCategoryElement();
+    this.mediumCategoryElement.innerHTML = this.createMediumCategoryElement();
+    this.smallCategoryElement.innerHTML = this.createSmallCategoryElement();
   }
 }
 
