2745024d21a9f9fb40eae251724038610351a4b9
puba.yu <puba.yu@kakaocorp.com>
2021-01-19 17:57:23 +0900

    feat : 마우스 올려 놓는 기능 추가
    - 마우스를 올려 놓을 때, 빠르게 움직이면 그 경로에 있는 것들 무시
    - 이는 마우스의 x,y 좌표를 이용하여 이동 속도를 계산하여 적용

--- public/js/components/menu.js
+++ public/js/components/menu.js
@@ -1,14 +1,40 @@
-import { $ } from '../utils.js';
+import { $, deleteClassFromElement } from '../utils.js';
 
 const LARGE_CATEGORY = ['패션 / 뷰티', '가전 / 컴퓨터', '가구 / 생활 / 건강'];
-const MEDIUM_CATEGORY = ['패션 / 뷰티', '가전 / 컴퓨터', '가구 / 생활 / 건강'];
-const SMALL_CATEGORY = ['패션 / 뷰티', '가전 / 컴퓨터', '가구 / 생활 / 건강'];
+const MEDIUM_CATEGORY = [
+  '패션 / 뷰티',
+  '가전 / 컴퓨터',
+  '가구 / 생활 / 건강',
+  '패션 / 뷰티',
+  '가전 / 컴퓨터',
+  '가구 / 생활 / 건강',
+  '패션 / 뷰티',
+  '가전 / 컴퓨터',
+  '가구 / 생활 / 건강',
+  '패션 / 뷰티',
+  '가전 / 컴퓨터',
+  '가구 / 생활 / 건강',
+];
+const SMALL_CATEGORY = [
+  '패션 / 뷰티',
+  '가전 / 컴퓨터',
+  '가구 / 생활 / 건강',
+  '패션 / 뷰티',
+  '가전 / 컴퓨터',
+  '가구 / 생활 / 건강',
+  '패션 / 뷰티',
+  '가전 / 컴퓨터',
+  '가구 / 생활 / 건강',
+];
 
 class Menu {
   constructor() {
     this.largeCategoryElement = $('.large-category');
     this.mediumCategoryElement = $('.medium-category');
     this.smallCategoryElement = $('.small-category');
+    this.activatedTab = $('.large-category').children[0];
+    this.currentX = 0;
+    this.currentY = 0;
   }
 
   createLargeCategoryElement() {
@@ -29,10 +55,30 @@ class Menu {
     }, '');
   }
 
+  calculateMouseMovement(newX, newY) {
+    return (
+      Math.pow(newX - this.currentX, 2) + Math.pow(newY - this.currentY, 2)
+    );
+  }
+
+  addCurrentTabEvent() {
+    $('.menu__pop-up').addEventListener('mousemove', event => {
+      if (this.calculateMouseMovement(event.clientX, event.clientY) < 5) {
+        deleteClassFromElement(this.activatedTab, 'tab__activated');
+        this.activatedTab = event.target;
+        this.activatedTab.classList.add('tab__activated');
+      }
+      this.currentX = event.clientX;
+      this.currentY = event.clientY;
+    });
+  }
+
   init() {
     this.largeCategoryElement.innerHTML = this.createLargeCategoryElement();
     this.mediumCategoryElement.innerHTML = this.createMediumCategoryElement();
     this.smallCategoryElement.innerHTML = this.createSmallCategoryElement();
+    console.log(this.activatedTab);
+    this.addCurrentTabEvent();
   }
 }
 
