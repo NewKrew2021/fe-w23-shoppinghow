29c9eff289e5ad52a4d591784482a57dc88fb11d
Kevin home <kevin.home@kakaocorp.com>
2021-01-20 13:18:38 +0900

    category mouseover event handler

--- public/js/component/categorycomponent.js
+++ public/js/component/categorycomponent.js
@@ -1,8 +1,14 @@
 import {myDomApi} from "../util/mydomapi.js"
 import {URL} from "../url.js"
 
-let [depthIdxFirst, depthIdxSecond] = [0, 0];
-let categoryData, categoryFirst, categorySecond, categoryThird;
+let depthFirst, depthSecond, categoryData, categoryFirst, categorySecond, categoryThird, dataObject = {};
+
+let firstClass = "category-first";
+let firstClickClass = "category-first category-first-click";
+let firstBorderClass = "category-first category-first-border";
+let secondClass = "category-second";
+let secondClickClass = "category-second category-second-click";
+let thirdClass = "category-third";
 
 let categoryContainer = myDomApi.myQuerySelector("div.category-layer");
 const categoryBtn = myDomApi.myQuerySelector("button.layer-btn");
@@ -32,44 +38,73 @@ const requestCategoryItem = () => {
   const request = new Request(URL + "/category");
   fetch(request)
   .then(response => response.text())
-  .then(result => {
-    categoryData = JSON.parse(result)["data"];
-    displayCategory();
-  })
+  .then(result => makeDataObject(result))
+  .then(displayCategory)
   .catch(error => console.log('error', error));
 }
 
+
+const makeDataObject = (result) => {
+  categoryData = JSON.parse(result)["data"];
+  let firstList = [];
+  for(let first=0; first<categoryData.length; first++) {
+    let secondList = [];
+    for(let second=0; second<categoryData[first].data.length; second++) {
+      let ThirdList = [];
+      for(let third=0; third<categoryData[first].data[second].data.length; third++) {
+        ThirdList.push(categoryData[first].data[second].data[third].title)
+      }
+      secondList.push(categoryData[first].data[second].title)
+      dataObject[categoryData[first].data[second].title] = ThirdList;
+    }
+    firstList.push(categoryData[first].title);
+    dataObject[categoryData[first].title]=secondList;
+  }
+  dataObject["root"] = firstList;
+  depthFirst = categoryData[0].title;
+  depthSecond = categoryData[0].data[0].title;
+  // console.log(depthFirst, depthSecond);
+  // console.log(dataObject);
+}
+
 const displayCategory = () => {
-  const categoryDataLen = [
-    categoryData.length,
-    categoryData[depthIdxFirst].data.length,
-    categoryData[depthIdxFirst].data[depthIdxSecond].data.length
-  ]
-  for(let idx=0; idx<categoryDataLen[0]; idx++) {
-    if(depthIdxFirst===idx) categoryFirst.innerHTML += `<div class="category-first category-first-click">${categoryData[idx].title}</div>`;
-    else categoryFirst.innerHTML += `<div class="category-first">${categoryData[idx].title}</div>`;
-  }
-  for(let idx=0; idx<categoryDataLen[1]; idx++) {
-    if(depthIdxSecond===idx) categorySecond.innerHTML += `<div class="category-second category-second-click">${categoryData[depthIdxFirst].data[idx].title}</div>`
-    else categorySecond.innerHTML += `<div class="category-second">${categoryData[depthIdxFirst].data[idx].title}</div>`
-  }
-  for(let idx=0; idx<categoryDataLen[2]; idx++) {
-    categoryThird.innerHTML += `<div class="category-second">${categoryData[depthIdxFirst].data[depthIdxSecond].data[idx].title}</div>`
-  }
+  console.log(depthFirst, depthSecond)
+  let [newHtmlFirst, newHtmlSecond, newHtmlThird]=[``, ``, ``];
+  Array.prototype.forEach.call(dataObject["root"], data => {
+    if(depthFirst===data) {
+      if(data === dataObject["root"][0]) newHtmlFirst += `<div class="${firstBorderClass}">${data}</div>`;
+      else newHtmlFirst += `<div class="${firstClickClass}">${data}</div>`;
+    }
+    else newHtmlFirst += `<div class="${firstClass}">${data}</div>`;
+  })
+  Array.prototype.forEach.call(dataObject[depthFirst], data => {
+    if(depthSecond===data) newHtmlSecond += `<div class="${secondClickClass}">${data}</div>`
+    else newHtmlSecond += `<div class="${secondClass}">${data}</div>`
+  })
+  Array.prototype.forEach.call(dataObject[depthSecond], data => {
+    newHtmlThird += `<div class="${thirdClass}">${data}</div>`;
+  })
+  categoryFirst.innerHTML = newHtmlFirst;
+  categorySecond.innerHTML = newHtmlSecond;
+  categoryThird.innerHTML = newHtmlThird;
 }
 
 const categoryHover = () => {
-  // categoryContainer.addEventListener("onclick", event => {
-  //   console.log(event.target);
-  //   if(event.target.class==="category-first"){
-  //     for(let idx=0; idx<categoryDataLen[0]; idx++) {
-  //       // if(event.target.class===) 
-  //     }
-  //   }
-  //   else{
-
-  //   }
-  // })
+  categoryContainer.addEventListener("mouseover", event => {
+    const targetClassName = event.target.className;
+    if(targetClassName===firstClass || targetClassName===firstClickClass || targetClassName===firstBorderClass){
+      depthFirst = event.target.innerHTML;
+      depthSecond = dataObject[depthFirst][0];
+      displayCategory();
+    }
+    else if(targetClassName===secondClass || targetClassName===secondClickClass){
+      depthSecond = event.target.innerHTML;
+      displayCategory();
+    }
+    else {
+    }
+    
+  })
 }
 
 export {createCategoryContainer}
