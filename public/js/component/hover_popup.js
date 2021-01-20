4fccb6d0ee668d1b8233569a738b1a5f770a6720
Geonho Kim <gunhoflash@gmail.com>
2021-01-20 18:51:52 +0900

    Refactor hover_popup.js

--- public/js/component/hover_popup.js
+++ public/js/component/hover_popup.js
@@ -2,39 +2,44 @@ import { find, findOne } from "../util"
 
 const POPUP_HIDE_DELAY = 250
 
+// mouse-enter event listener
+function enterHoverPopupTrigger(event) {
+  const popupTarget = findOne(event.target.dataset.hoverPopupTarget)
+
+  // clear setTimeout handler
+  const setTimeoutHandle = popupTarget.dataset.hoverSetTimeout
+  if (setTimeoutHandle) {
+    clearTimeout(setTimeoutHandle)
+    delete popupTarget.dataset.hoverSetTimeout
+  }
+
+  // show the target
+  setTimeout(() => {
+    popupTarget.classList.add('show')
+  }, 0)
+}
+
+// mouse-leave event listener
+function leaveHoverPopupTrigger(event) {
+  const popupTarget = findOne(event.target.dataset.hoverPopupTarget)
+
+  // directly hide
+  if (popupTarget.dataset.hoverSetTimeout == 0) {
+    popupTarget.classList.remove('show')
+    return
+  }
+  // or, set setTimeout handler
+  popupTarget.dataset.hoverSetTimeout = setTimeout(() => {
+    // hide the target after {POPUP_HIDE_DELAY}ms
+    popupTarget.classList.remove('show')
+    delete popupTarget.dataset.hoverSetTimeout
+  }, popupTarget.dataset.hoverHideDelay || POPUP_HIDE_DELAY)
+}
+
 export function initHoverPopup() {
-  find('[data-hover-popup-target]')
-    .forEach(element => {
-      const popupTarget = findOne(element.dataset.hoverPopupTarget)
-
-      // add mouse-enter event listener
-      element.addEventListener('mouseenter', event => {
-        // clear setTimeout handler
-        const setTimeoutHandle = popupTarget.dataset.hoverSetTimeout
-        if (setTimeoutHandle) {
-          clearTimeout(setTimeoutHandle)
-          delete popupTarget.dataset.hoverSetTimeout
-        }
-
-        // show the target
-        setTimeout(() => {
-          popupTarget.classList.add('show')
-        }, 0)
-      })
-
-      // add mouse-leave event listener
-      element.addEventListener('mouseleave', event => {
-        // directly hide
-        if (popupTarget.dataset.hoverSetTimeout == 0) {
-          popupTarget.classList.remove('show')
-          return
-        }
-        // or, set setTimeout handler
-        popupTarget.dataset.hoverSetTimeout = setTimeout(() => {
-          // hide the target after {POPUP_HIDE_DELAY}ms
-          popupTarget.classList.remove('show')
-          delete popupTarget.dataset.hoverSetTimeout
-        }, popupTarget.dataset.hoverHideDelay || POPUP_HIDE_DELAY)
-      })
-    })
+  find('[data-hover-popup-target]').forEach(element => {
+    // add mouse-enter & mouse-leave event listeners
+    element.addEventListener('mouseenter', enterHoverPopupTrigger)
+    element.addEventListener('mouseleave', leaveHoverPopupTrigger)
+  })
 }
