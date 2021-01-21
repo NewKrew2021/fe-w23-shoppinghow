444fab4069e743f9176de79492fea901d2e8f340
Geonho Kim <gunhoflash@gmail.com>
2021-01-21 11:21:40 +0900

    Show recommended keywords

--- public/js/component/searchbar.js
+++ public/js/component/searchbar.js
@@ -1,5 +1,33 @@
 import { findOne, myFetchPOST } from "../util"
 
+function initRecommendBox(box, data) {
+  // {data} is array of recommended keywords(string)
+
+  let htmlString = ''
+
+  if (data.length > 0) {
+    // open list
+    htmlString += `<ul class="list-no-style">`
+
+    // append list items
+    htmlString += data.reduce((htmlString, word) => htmlString + `
+      <li>
+        <a href="#" class="d-block hover-bg-bright px-4 py-2 text-small">
+          ${word}
+        </a>
+      </li>
+    `, '')
+
+    // close list
+    htmlString += `</ul>`
+  } else {
+    // no data
+    htmlString += `<p class="gray p-4">Nothing to recommend<p>`
+  }
+
+  box.innerHTML = htmlString 
+}
+
 // init search bar
 export function initSearchBar() {
   const input_search = findOne('#input_search')
@@ -7,7 +35,7 @@ export function initSearchBar() {
   const search_recommend = findOne('#search_recommend')
   const hot_search = findOne('#hot_search')
 
-  input_search.addEventListener('input', (e) => {
+  input_search.addEventListener('input', async (e) => {
     const keyword = input_search.value.trim() || '';
 
     // handle exception: no keyword
@@ -15,9 +43,8 @@ export function initSearchBar() {
       return
     }
 
-    // fetch recommended keywords
-    myFetchPOST('/search/recommend', { keyword })
-    .then(console.log)
-    .catch(console.log)
+    // fetch and show recommended keywords
+    const recommendData = await myFetchPOST('/search/recommend', { keyword })
+    initRecommendBox(search_recommend, recommendData)
   })
 }
