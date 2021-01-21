47fdd72b629ca4e7bdd2908082fc9f76ef50d395
Kevin home <kevin.home@kakaocorp.com>
2021-01-21 12:26:14 +0900

    category mouse move effect

--- public/js/component/InputView.js
+++ public/js/component/InputView.js
@@ -1,12 +1,15 @@
 import {myDomApi} from "../util/MyDomApi.js"
 import {URL} from "../url.js"
 
+let inputItem;
 const inputCycleTime = 1500; //ms
+const inputDefault = ["1   부라타치즈","2   무스탕코트","3   오덴세시손느",
+                      "4   구강세척기","5   게이밍의자", "6   현관코일매트",
+                      "7   여성등산화","8   에어프라이어","9   접이식욕조",
+                      "10   글라스텐다지기"]
 
 const createInputContainer = () => {
   let time=1;
-  const inputDefault = ["1   부라타치즈","2   무스탕코트","3   오덴세시손느","4   구강세척기","5   게이밍의자",
-                  "6   현관코일매트","7   여성등산화","8   에어프라이어","9   접이식욕조","10   글라스텐다지기"]
   const input = myDomApi.myQuerySelector("input.search-input");
   const changeInput = setInterval(()=> {
     input.value = inputDefault[time];
@@ -17,6 +20,19 @@ const createInputContainer = () => {
     clearInterval(changeInput);
     input.value="";
   });
+  requestInputItem();
+}
+
+const requestInputItem = () => {
+  const request = new Request(URL + "/input");
+  fetch(request)
+  .then(response => response.text())
+  .then(result => {
+    inputItem = JSON.parse(result)["data"]
+    console.log(inputItem)
+  })
+  .catch(error => console.log('error', error));
+  
 }
 
 export {createInputContainer}
