4446e9c2bb40065ef30ad54a3cd9991f6a618117
Geonho Kim <gunhoflash@gmail.com>
2021-01-21 14:04:11 +0900

    Show hot keywords below the searchbar

--- public/js/component/searchbar.js
+++ public/js/component/searchbar.js
@@ -1,50 +1,100 @@
-import { findOne, myFetchPOST } from "../util"
+import { findOne, myFetchGET, myFetchPOST } from "../util"
+
+const htmlElements = {}
 
 function initRecommendBox(box, data) {
   // {data} is array of recommended keywords(string)
 
-  let htmlString = ''
-
-  if (data.length > 0) {
-    // open list
-    htmlString += `<ul class="list-no-style">`
-
-    // append list items
-    htmlString += data.reduce((htmlString, word) => htmlString + `
+  function getListItemHtmlString(text) {
+    return `
       <li>
-        <a href="#" class="d-block hover-bg-bright px-4 py-2 text-small">
-          ${word}
+        <a href="#" class="d-block hover-bg-bright px-4 py-2">
+          ${text}
         </a>
-      </li>
-    `, '')
-
-    // close list
-    htmlString += `</ul>`
-  } else {
-    // no data
-    htmlString += `<p class="gray p-4">Nothing to recommend<p>`
+      </li>`
   }
 
-  box.innerHTML = htmlString 
+  if (data.length > 0) {
+    // append list items
+    box.innerHTML = `
+      <ul class="list-no-style">
+        ${data.reduce((htmlString, word) => htmlString + getListItemHtmlString(word), '')}
+      </ul>`
+  } else {
+    // no data
+    box.innerHTML = `<p class="gray p-4">Nothing to recommend<p>`
+  }
+}
+
+function initHotSearchBox(box, data) {
+  // {data} is array of hot keywords(string)
+
+  function getListItemHtmlString(i, text) {
+    return `
+      <li>
+        <a href="#" class="d-block py-2">
+          <strong>${i}</strong> ${text}
+        </a>
+      </li>`
+  }
+
+  function getListPartialHtmlString(data, start, end) {
+    let i = start + 1
+    return data.slice(start, end).reduce((htmlString, word) => 
+      htmlString + getListItemHtmlString(i++, word), '')
+  }
+
+  // append hot keywords
+  box.innerHTML = `
+    <div class="px-4">
+      <p>
+        <strong class="gray">
+          인기 쇼핑 키워드
+        </strong>
+      </p>
+      <ol class="d-flex list-no-style">
+        <div class="flex-1">
+          ${getListPartialHtmlString(data, 0, 5)}
+        </div>
+        <div class="flex-1">
+          ${getListPartialHtmlString(data, 5, 10)}
+        </div>
+      </ol>
+    </div>`
+}
+
+async function inputEventHandler() {
+  const keyword = input_search.value.trim() || '';
+
+  // no keyword
+  if (!keyword) {
+    // show hot search keywords
+    htmlElements.search_recommend.classList.add('d-none')
+    htmlElements.hot_search.classList.remove('d-none')
+    return
+  } else {
+    // show recommend keywords
+    htmlElements.hot_search.classList.add('d-none')
+    htmlElements.search_recommend.classList.remove('d-none')
+  }
+
+  // fetch and show recommended keywords
+  const recommendData = await myFetchPOST('/search/recommend', { keyword })
+  initRecommendBox(htmlElements.search_recommend, recommendData)
 }
 
 // init search bar
-export function initSearchBar() {
-  const input_search = findOne('#input_search')
-  const search_helper = findOne('#search_helper')
-  const search_recommend = findOne('#search_recommend')
-  const hot_search = findOne('#hot_search')
+export async function initSearchBar() {
+  htmlElements.input_search = findOne('#input_search')
+  htmlElements.search_helper = findOne('#search_helper')
+  htmlElements.search_recommend = findOne('#search_recommend')
+  htmlElements.hot_search = findOne('#hot_search')
 
-  input_search.addEventListener('input', async (e) => {
-    const keyword = input_search.value.trim() || '';
+  // show suggestion
+  htmlElements.input_search.addEventListener('input', inputEventHandler)
+  htmlElements.input_search.addEventListener('focus', inputEventHandler)
 
-    // handle exception: no keyword
-    if (!keyword) {
-      return
-    }
-
-    // fetch and show recommended keywords
-    const recommendData = await myFetchPOST('/search/recommend', { keyword })
-    initRecommendBox(search_recommend, recommendData)
-  })
+  // show hot-searched keywords
+  const hotSearchedData = await myFetchGET('/search/hot')
+  initHotSearchBox(htmlElements.hot_search, hotSearchedData)
 }
